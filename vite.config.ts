4e5d61f744f01a828419ce9e65ae2f17450a8365
liuqiang <liuqiang1357@163.com>
2023-08-17 11:07:18 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   define: {
-    global: 'window',
+    global: 'globalThis',
   },
   plugins: [react(), tsconfigPaths()],
 });
